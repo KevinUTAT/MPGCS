4d157769653cd7d4c7d1c1119589ab9f0f716319
KevinUTAT <wenkai.xu@mail.utoronto.ca>
2017-07-11 15:50:34 -0400

    not done
    starting on a parser, and usuable help command

--- MPGCS.cpp
+++ MPGCS.cpp
@@ -1,11 +1,72 @@
-// MPGCS.cpp : Defines the entry point for the console application.
-//
+/* ================================================================
+   Project Name:	MPGCS
+   Entry Point:  	MPGCS.cpp (This File)
+   Author:       	Kevin Xu
+   Contact: 		kevin@dronedeliverycanada.ca
+   Data:    		2017/07/11
+   ----------------------------------------------------------------
+   Copyright 2017, Drone Delivery Canada Inc. All right reserved
+   ================================================================
+*/
 
+#include <iostream>
+#include <sstream>
+#include <string>
+#include "actions.h"
 #include "stdafx.h"
+#include "mpfields.h"
+#include "c-multi.h"
+
+using namespace std;
+
+
+void parser() {
+	
+	while (true)
+	{
+		cout << "> ";
+		stringstream commandBuf;
+		commandBuf  cin;
+		string paramBuf0;
+		string paramBuf1;
+		getline(commandBuf, paramBuf0);
+		if (commandBuf.fail()) {
+			cout << "Input error" << endl;
+		}
+// ---------------------------- help ----------------------------
+		if (paramBuf0.compare("help") == 0) command_list();
+
+// ---------------------------- exit ----------------------------
+		else if ((paramBuf0.compare("exit") == 0)
+			|| (paramBuf0.compare("quit") == 0)) {
+			break;
+		}
+
+// --------------------------- connect --------------------------
+		else if (paramBuf0.compare("connect") == 0) {
+			getline(commandBuf, paramBuf1);
+			if (commandBuf.fail()) {
+				cout << "Error: 0" << endl;
+			}
+			if (commandBuf.eof()) {
+				cout << "Error: 1" << endl;
+			}
+			continue;
+		}
+
+		else {
+			cout << "Error: illegal command" << endl;
+			continue;
+		}
+	}
+}
 
 
 int main()
 {
+	cout << "This is MP-GCS V0.1" << endl;
+	cout << "Enter \"help\" for full command list" << endl;
+	parser();
     return 0;
 }
 
---
+++ actions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include "c-multi.h"
+
+using namespace std;
+
+void command_list() {
+	cout << "Full Commands List \n"
+		<< "Command		Parameter	Action \n"
+		<< "-------------------------------------------------------\n"
+		<< "help		<void>		show full commands list\n"
+		<< "exit		<void>		exit GCS\n"
+		<< "quit		<void>		exit GCS\n"
+		<< endl;
+}
